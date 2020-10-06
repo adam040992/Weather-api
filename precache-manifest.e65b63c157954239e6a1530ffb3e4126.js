401f280d4038f857178473a09ad6d9ff4e1ed07f
Adam Walter <adam040992@gmail.com>
2020-10-06 19:39:22 +0200

    Updates

---
+++ precache-manifest.e65b63c157954239e6a1530ffb3e4126.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067bb2c2ee317c97e806c24a475b79bd",
+    "url": "/Weather-api/index.html"
+  },
+  {
+    "revision": "53c286ed48713c02a882",
+    "url": "/Weather-api/static/css/main.ae5cac04.chunk.css"
+  },
+  {
+    "revision": "5a85634a736f51b04e32",
+    "url": "/Weather-api/static/js/2.a02d2411.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-api/static/js/2.a02d2411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c286ed48713c02a882",
+    "url": "/Weather-api/static/js/main.8f61c5cc.chunk.js"
+  },
+  {
+    "revision": "1d38fd213a26cd4615ba",
+    "url": "/Weather-api/static/js/runtime-main.3f30827d.js"
+  },
+  {
+    "revision": "8bdf2e7185d3ea23bd47dc6d30e7dec6",
+    "url": "/Weather-api/static/media/cloud.8bdf2e71.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Weather-api/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "186e8f6b3b9269d65445e27acee7c168",
+    "url": "/Weather-api/static/media/mountains2.186e8f6b.jpg"
+  },
+  {
+    "revision": "6da4dd1db287dbef41f1fe36374ef6af",
+    "url": "/Weather-api/static/media/sunny.6da4dd1d.svg"
+  },
+  {
+    "revision": "5027b830e06475a1f5f025cc1ab30ce0",
+    "url": "/Weather-api/static/media/wi-wind-deg2.5027b830.svg"
+  }
+]);
